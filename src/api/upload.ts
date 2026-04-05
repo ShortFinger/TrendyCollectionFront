3bc3c19b0c0c47060deafaa3b682738dc0947c83
Lsssx <16547510+lsssx@user.noreply.gitee.com>
2026-04-06 03:21:29 +0800

    chore(front): mark legacy upload API as deprecated

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,6 +1,9 @@
 import request from '@/utils/request'
 import type { Result } from '@/types/api'
 
+/**
+ * @deprecated 请使用 src/utils/oss.ts 中的 uploadFile 或 uploadCmsFile
+ */
 export function uploadImage(file: File) {
   const fd = new FormData()
   fd.append('file', file)
